573b0f1dbb25f2b8060eb829cc3a3d721d9c76f8
root <root@localhost.localdomain>
2019-12-02 17:52:19 +0000

    safhöo

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
 git pull
-screen -d -m -S "minecraft" java -jar /usr/share/mc/winterprojekt/spigot-1.14.4.jar
-git add .
-git commit -m "played"
-git push
+screen -d -m -S "minecraft" java -jar /usr/share/mc/winterprojekt/spigot-1.14.4.jar && git add . && git commit -m "played" && git push
